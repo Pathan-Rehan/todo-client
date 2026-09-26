71c62b31463b5bc58197127da765bab70181e2a9
Pathan Rehan <pathanrehankhan68@gmail.com>
2026-09-26 16:54:26 +0530

    Mongodb databse is connected to backend

--- todo-server/server.js
+++ todo-server/server.js
@@ -1,9 +1,16 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import dns from 'dns';
 
+dns.setServers(["1.1.1.1","8.8.8.8"]);
+dotenv.config();
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Todo API is running' });
+
+await connectDB();
+app.get("/", (req, res) => {
+  res.send({ message: "Todo API is running" });
 });
 
 const PORT = 5000;
